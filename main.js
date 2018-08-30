3d9c86d264fef643e1664c088ede621e9bc93ecb
Mark Judy <mrkjudy@gmail.com>
2018-08-30 15:40:21 -0400

    updated main.js to use helloWorld.html

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => {res.send('Hello World!')})
+  .get('/', function(req, res){ res.sendFile(__dirname+'/helloWorld.html');})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
