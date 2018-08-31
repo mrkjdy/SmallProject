b75f84dbfd2efded4d88fc600cdbda23000a9c85
Mark Judy <mrkjudy@gmail.com>
2018-08-31 00:47:39 -0400

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const express = require('express');
-//const path = require('path')
+const express = require("express");
 const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-app.get('/', function(req, res){ res.sendFile(__dirname+'/helloWorld.html');});
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, function()
+{
+	console.log("Listening on " + PORT)
+});
+
+app.get('/', function(req, res)
+{ 
+	res.sendFile(__dirname + "/helloWorld.html");
+});
