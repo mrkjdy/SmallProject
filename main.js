4afc18157978e3cabbd3b00db96fc5b3475d66ef
Mark Judy <mrkjudy@gmail.com>
2018-08-31 17:03:37 -0400

    fixed database connection closing

--- main.js
+++ main.js
@@ -2,18 +2,49 @@ const express = require("express");
 const mysql = require("mysql");
 const PORT = process.env.PORT || 5000;
 
-var connection = mysql.createConnection({
+
+// mysql stuff
+var db_config = {
   host     : 'us-cdbr-iron-east-01.cleardb.net',
   user     : 'b0e7c31b916c42',
   password : '04f5efab',
   database : 'heroku_883b37654a02d69'
-});
+};
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+var connection;
 
+function handleDisconnect() 
+{
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) 
+  {
+    if(err) 
+    {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+    console.log("Connected to mysql database!")
+  });
+
+  connection.on('error', function(err) 
+  {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') 
+    {
+      handleDisconnect();
+    }
+    else 
+    {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+
+// Express stuff
 var app = express();
 
 app.use(express.static(__dirname));
@@ -22,8 +53,3 @@ app.listen(PORT, function()
 {
 	console.log("Listening on " + PORT)
 });
-
-// app.get('/', function(req, res)
-// { 
-// 	res.sendFile(__dirname + "/index.html");
-// });
