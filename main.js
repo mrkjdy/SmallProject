7a85c1a1769ecdd28c27671282eec477aa3c961a
Mark Judy <mrkjudy@gmail.com>
2018-08-31 14:31:48 -0400

    testing mysql connection

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const express = require("express");
 const mysql = require("mysql");
 const PORT = process.env.PORT || 5000;
 
+var con = mysql.createConnection({
+  host: "smallproject.com",
+  user: "Group4UCF",
+  password: "Cop4331ucf"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 var app = express();
 
 app.use(express.static(__dirname));
