d31933531906e1cb7902d46854a9b0b816956e52
Mark Judy <mrkjudy@gmail.com>
2018-08-31 13:38:22 -0400

    testing index.html

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 
+app.use(express.static(__dirname));
+
 app.listen(PORT, function()
 {
 	console.log("Listening on " + PORT)
 });
 
-app.get('/', function(req, res)
-{ 
-	res.sendFile(__dirname + "/helloWorld.html");
-});
+// app.get('/', function(req, res)
+// { 
+// 	res.sendFile(__dirname + "/index.html");
+// });
