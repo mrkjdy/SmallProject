017a4c37b94e7d01470b8799c7215b7f80413067
Mark Judy <mrkjudy@gmail.com>
2018-08-31 00:18:53 -0400

    testing changes to main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+//const path = require('path')
+const PORT = process.env.PORT || 5000;
 
-express()
-  .get('/', function(req, res){ res.sendFile(__dirname+'/helloWorld.html');})
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.get('/', function(req, res){ res.sendFile(__dirname+'/helloWorld.html');});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
