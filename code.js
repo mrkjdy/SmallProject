1bb68fe7a7a7be3405adb82cd95639f05401d479
manu <manumanu-96@hotmail.com>
2018-09-06 12:21:02 -0400

    added logic to search contact

--- code.js
+++ code.js
@@ -118,7 +118,7 @@ function searchContact()
 	// Create JSON pacage and send it to API
 	var jsonPayload = '{"firstname" : "' + fName + '", "lastname" : "'
 						+ lName + '"}';
-	var url = urlBase + '/SearchContact' + fileExtension;
+	var url = APIRoot + '/SearchContact' + fileExtension;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -130,11 +130,35 @@ function searchContact()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
+				var table = document.getElementById("cTable");
+
+				// clear the table
+				while (table.length > 1)
+				{
+					table.deleteRow(table.length - 1);
+				}
+
+				var jsonObject = JSON.parse(xhr.responseText);
+
+				// create the fields in the table 
+				// for(var i = 0; i < array.length; i++)
+				// {
+				// 	// create a new row
+				// 	var newRow = table.insertRow(table.length);
+				// 	for(var j = 0; j < array[i].length; j++)
+				// 	{
+				// 		// create a new cell
+				// 		var cell = newRow.insertCell(j);
+
+				// 		// add value to the cell
+				// 		cell.innerHTML = array[i][j];
+				// 	}
+				// }
 			}
 		};
 		xhr.send(jsonPayload);
 	}
-	catch
+	catch(err)
 	{
 		
 	}
@@ -156,6 +180,25 @@ function deleteContact(index)
 	var pNum = document.getElementById("cTable").rows[index].cells[3].innerHTML;
 
 	// Create JSON pacage and send it to API
+	var jsonPayload = '{"firstname" : "' + fName + '", "lastname" : "'
+						+ lName + '", "email" : "' + eMail 
+						+ '", "phone" : "' + pNum + '"userid" : "' 
+						+ userID + '"}';
+
+	var url = APIRoot + '/DeleteContact' + fileExtension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+
+	}
 	// End
 
 	// Update the table on the website
@@ -179,14 +222,15 @@ function logout()
 
 function createAccount()
 {
+	var user = document.getElementById("newUName").value;
 	var newPWord1 = document.getElementById("newPWord1").value;
 	var newPWord2 = document.getElementById("newPword2").value;
-	
-	// Check if username available
 
-		// if not available print status message
-		// document.getElementById("submitMessage").innerHTML = "Username not available";
-		// return;
+	if (user == "")
+	{
+		document.getElementById("submitMessage").innerHTML = "Enter an user name";
+		return;
+	}
 
 	// Check if passwords match
 	if (newPWord1 != newPWord2)
@@ -195,11 +239,31 @@ function createAccount()
 		return;
 	}
 
+	// Check if username available
+	var jsonPayload = '{"username" : "' + user + '", "password" : "' + newPWord1 + '"}';
+	var url = APIRoot + '/register.' + fileExtension;
 
-	// Create the account
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		xhr.send(jsonPayload);
+
+		if (this.status == 400)
+		{
+			// change the user
+			return;
+		}
+	}
+	catch(err)
+	{
+		// return error mesage
+	}
 
 	// set username and password then login
-	document.getElementById("uName").value = document.getElementById("newUName").value;
+	document.getElementById("uName").value = user;
 	document.getElementById("pWord").value = newPWord1;
 	login();
 }
