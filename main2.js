cf84ebdb40cbb51a96e8ccd57df24f1e00943f41
manu <manumanu-96@hotmail.com>
2018-09-07 14:42:04 -0400

    some API changes

--- main2.js
+++ main2.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const PORT = process.env.PORT || 5000;
 
 const server = express();
 
@@ -78,8 +79,7 @@ server.post('/register', function(req, res) {
 server.post('/addcontact', function(req, res) { 
 
 	// Check if correct format
-	if(checkInput(req.body.firstname, "name") && checkInput(req.body.lastname, "name")&& checkInput(req.body.state, "name") && checkInput(req.body.city, "name") &&
-				checkInput(req.body.phone, "phone") && checkInput(req.body.email, "email") && checkInput(req.body.street, "street") && checkInput(req.body.zip, "zip")) {
+	if(checkInput(req.body.firstname, "name") && checkInput(req.body.lastname, "name") && checkInput(req.body.phone, "phone") && checkInput(req.body.email, "email")) {
 			
 		// Create connection to database
 		db.getConnection(function(err, tempCont){
@@ -91,9 +91,8 @@ server.post('/addcontact', function(req, res) {
 			} else {
 				
 				// Add contact to database, Might need a parseInt method if UserId is parsed as a string
-				const sqlAddContact = "INSERT INTO contact (UserId, FirstName, LastName, PhoneNumber, Email, Address, City, State) VALUES (";
-				tempCont.query(sqlAddContact + req.body.UserId + ", '" + req.body.firstname + "', '" + req.body.lastname + "', '" + req.body.phone + "', '" + req.body.email + "', '" + req.body.address + "', '" 
-										+ req.body.city + req.body.state + "')", function(err, result){
+				const sqlAddContact = "INSERT INTO contact (UserId, FirstName, LastName, PhoneNumber, Email) VALUES (";
+				tempCont.query(sqlAddContact + req.body.UserId + ", '" + req.body.firstname + "', '" + req.body.lastname + "', '" + req.body.phone + "', '" + req.body.email + "')", function(err, result){
 					
 					// Check if query works
 					if (err) {
@@ -212,85 +211,83 @@ server.post('/searchcontact', function(req, res) {
 				// Searching by phone number
 				case "phone":
 				
-					
+					// Check if correct format
 					if(checkInput(req.body.value, req.body.type)) {
+						
+						// Search for phone number in database
 						tempCont.query("SELECT * FROM contact WHERE UserId = ? AND PhoneNumber = ?",[req.body.UserId, req.body.phone], function(err, result){
+							
+							// Check if query works
 							if (err) {
 								res.status(400).send('Query Fail');
-					
 							} else {
 								res.send(result);
-						
 							}
 				        });
 				
 					} else {
 						res.status(400).send('Invalid Values');
-
 					}
 					
 					break;
 					
-				// Default is set to check by email
+				// Searching by email
 				default:
+				
+					// Check if correct format
 					if(checkInput(req.body.value, req.body.type)) {
+						
+						// Search for email in database
 						tempCont.query("SELECT * FROM contact WHERE UserId = ? AND Email = ?",[req.body.UserId, req.body.email], function(err, result) {
+							
+							// Check if query works
 							if (err) {
 								res.status(400).send('Query Fail');
-					
 							} else {
 								res.send(result);
-						
-							}
-				        
+							}      
 				        });
 				
 					} else {
 						res.status(400).send('Invalid Values');
-
 					}
 					
 					break;
 			}
 		}
 		
+		// End connection
 		tempCont.release();
 	});	
 });
 
-//Confirm Input
+//Check for valid inputs
 var checkInput = function(input, type, callback) {
+	
 	var returnVal = null;
 	
 	switch(type) {
+		
 		case "username":
-			var re = /^[a-z|\d]{5,20}$/i; //Characters and digits
+			var re = /^[a-z|\d]{5,20}$/i; // Format 5-20 characters and digit
 			returnVal = re.test(input);
 			break;
 			
 		case "email":
-			var re = /^[a-z\d]{1,20}@[a-z]{1,10}(.[a-z]{3}){1,2}$/i; //aaaaa@bbbb.ccc.ddd
-			returnVal = re.test(input);
-			break;
-
-		case "street":
-			var re = /^\d{1,5}(\s[a-z]+)+$/i; //Can replace + with a {x,y} (11111 aaaaa bbbbb)
+			var re = /^[a-z\d]{1,20}@[a-z]{1,10}(.[a-z]{3}){1,2}$/i; // Format 1-20 character @ 1-10 characters . extension
 			returnVal = re.test(input);
 			break;
 			
-		case "zip":
-			var re = /^\d{5}$/i; //5 numbers
-			returnVal = re.test(input);
-			break;
-			
-		case "name": //firstname, lastname, state, city,
-			var re = /^[a-z]{1,20}$/i; //Characters only
+		case "name":
+			var re = /^[a-z]{1,20}$/i; // Format 20 characters
 			returnVal = re.test(input);
 			break;
 			
 		case "phone":
-			var re = /(1){0,1}(\d{3}\s{0,1}){2}\d{4}$/i; //Does not cover all cases (4074445555 | 407 444 555)
-			returnVal = re.test(input);
+			var re = /(1){0,1}\d{10}$/i; // Format 18004445555 | 4074445555
+			var number = input.replace(/[^\d]/g, ''));
+			
+			returnVal = re.test(number);
 			break;
 		
 		default:
@@ -306,4 +303,4 @@ var checkInput = function(input, type, callback) {
 	}
 }
 
-server.listen(8080);
+server.listen(PORT);
