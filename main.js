77816dc3806a3392a818db8aa23871652ac40398
John Mirschel <john@Johns-MacBook-Air.local>
2018-09-07 20:41:54 -0400

    Test

--- main.js
+++ main.js
@@ -231,13 +231,14 @@ app.post('/searchcontact', function(req, res) {
 						if(checkInput(req.body.value, "name")) {
 							
 							// Search for first name in database
-							tempCont.query("SELECT * FROM contact WHERE FirstName = '?'", [requset.body.firstname] function(err, result) {
+							tempCont.query("SELECT * FROM contact WHERE UserId = ? AND FirstName = ?",[req.body.userid, req.body.value], function(err, result) {
 								
 								// Check if query works
 								if (err) {
 									res.status(400).send('Query Fail');
 								} else {
-									res.send(result);									
+									res.send(result);
+									console.log(req.body.userid);
 								}				        
 							});				
 					
