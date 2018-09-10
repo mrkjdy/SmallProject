24dd1b507352a9d7828dacbcfec1ce2f5420feaa
John Mirschel <john@Johns-MacBook-Air.local>
2018-09-10 11:13:31 -0400

    Update Search

--- main.js
+++ main.js
@@ -30,44 +30,6 @@ var db = mysql.createPool({
 });
 
 
-// Login page
-app.post('/login', function(req, res) {
-	console.log('form submitted');
-	// Create connection to database
-	db.getConnection(function(err, tempCont){
-			
-		// Check if correct format
-		if(checkInput(req.body.value, "username")) {
-						
-			// Search for username and password in database
-			tempCont.query("SELECT UserId FROM users WHERE Login = ? AND Password = ?", [req.body.username, req.body.password], function(err, result) { //Check why bolded
-				
-				res.setHeader('Content-Type', 'application/json');
-
-				// Check if query works
-				if (err) {
-					res.status(400).send('Query Fail');
-				} else {
-						
-					// Check if username and password is in the database
-					if(result == "") {
-						res.send(JSON.stringify([{ "UserId": 0 }]));
-					} else {
-						res.send(result);
-					}	
-				}				        
-			});				
-				
-		} else {
-			res.status(400).send('Invalid Values');
-		}
-			
-		// End connection
-		tempCont.release();
-	
-	});
-});
-
 
 // Register page
 app.post('/register', function(req, res) {
@@ -127,204 +89,185 @@ app.post('/register', function(req, res) {
 // Add contact page
 app.post('/addcontact', function(req, res) { 
 	
-	// Check if user is logged on
-	if(req.body.userid < 1){
-		res.status(400).send('User not logged in');
-	} else {
-	
-		// Check if correct format
-		if(checkInput(req.body.firstname, "name") && checkInput(req.body.lastname, "name") && checkInput(req.body.phone, "phone") && checkInput(req.body.email, "email")) {
+	// Check if correct format
+	if(checkInput(req.body.firstname, "name") && checkInput(req.body.lastname, "name") && checkInput(req.body.phone, "phone") && checkInput(req.body.email, "email")) {
 			
-			// Create connection to database
-			db.getConnection(function(err, tempCont){
-			
-				// Error if connection is not established
-				if(err) {
-					res.status(400).send('Connection Fail');
-			
-				} else {
-				
-					// Add contact to database, Might need a parseInt method if UserId is parsed as a string
-					const sqlAddContact = "INSERT INTO contact (UserId, FirstName, LastName, PhoneNumber, Email) VALUES (";
-					tempCont.query(sqlAddContact + req.body.userid + ", '" + req.body.firstname + "', '" + req.body.lastname + "', '" + req.body.phone + "', '" + req.body.email + "')", function(err, result){
-					
-						// Check if query works
-						if (err) {
-							res.status(400).send('Query Fail');				
-						} else {
-							res.status(200).send('Query Success');					
-						}
-					
-						// End connection
-						tempCont.release();
-       
-					});        
-				}
-			});
-			
-		} else {
-			res.status(400).send('Invalid Values');
-		}
-	}
-});
-
-// Delete Contact
-app.post('/deletecontact', function(req, res) {
-	
-	// Check if user is logged on
-	if(req.body.userid < 1){
-		res.status(400).send('User not logged in');
-	} else {
-	
 		// Create connection to database
 		db.getConnection(function(err, tempCont){
 			
 			// Error if connection is not established
 			if(err) {
-				res.status(400).send('Connection fail');
-				
-			} else { 
+				res.status(400).send('Connection Fail');
 			
-				// Delect contact from database, Might need a parseInt method if UserId and ContactId is parsed as a string
-				const sqlDeleteContact = "Delete FROM contact WHERE UserID = ? AND ContactId = ?";
-				tempCont.query(sqlDeleteContact,[req.body.userid, req.body.contactid], function(err, result) {
+			} else {
+				
+				// Add contact to database, Might need a parseInt method if UserId is parsed as a string
+				const sqlAddContact = "INSERT INTO contact (UserID, FirstName, LastName, PhoneNumber, Email) VALUES (";
+				tempCont.query(sqlAddContact + req.user.UserID + ", '" + req.body.firstname + "', '" + req.body.lastname + "', '" + req.body.phone + "', '" + req.body.email + "')", function(err, result){
 					
 					// Check if query works
 					if (err) {
-						res.status(400).send('Query Fail');
+						res.status(400).send('Query Fail');				
 					} else {
-						res.status(200).send('Query Success');		
+						res.status(200).send('Query Success');					
 					}
 					
 					// End connection
 					tempCont.release();
-			
-				});
-				
+       
+				});        
 			}
-
 		});
+			
+	} else {
+		res.status(400).send('Invalid Values');
 	}
+});
+
+// Delete Contact
+app.post('/deletecontact', function(req, res) {
+
+	// Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else { 
+			
+			// Delect contact from database, Might need a parseInt method if UserID and ContactId is parsed as a string
+			const sqlDeleteContact = "Delete FROM contact WHERE UserID = ? AND ContactID = ?";
+			tempCont.query(sqlDeleteContact,[req.user.UserID, req.body.contactid], function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+				} else {
+					res.status(200).send('Query Success');		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+			});
+				
+		}
+
+	});
 });
 
 // Search Contact
 app.post('/searchcontact', function(req, res) {
-	
-	// Check if user is logged on
-	if(req.body.userid < 1){
-		res.status(400).send('User not logged in');
-	} else {
-	
-		// Create connection to database
-		db.getConnection(function(err, tempCont){
-			
-			// Error if connection was not established
-			if(err) {
-				res.status(400).send('Connection fail');
-				
-			} else {
-				
-				switch(req.body.type) {
-					
-					// Searching by first name
-					case "firstname":
-					
-						// Check if correct format
-						if(checkInput(req.body.value, "name")) {
-							
-							// Search for first name in database
-							tempCont.query("SELECT * FROM contact WHERE UserId = ? AND FirstName = ?",[req.body.userid, req.body.value], function(err, result) {
-								
-								// Check if query works
-								if (err) {
-									res.status(400).send('Query Fail');
-								} else {
-									res.send(result);
-								}				        
-							});				
-					
-						} else {
-							res.status(400).send('Invalid Values');
-						}	
-						
-						break;
-						
-					// Searching by last name
-					case "lastname":
-					
-						// Check if correct format
-						if(checkInput(req.body.value, "name")) {
-							
-							// Search for last name in database
-							tempCont.query("SELECT * FROM contact WHERE UserId = ? AND LastName = ?",[req.body.userid, req.body.value], function(err, result) {
-								
-								// Check if query fail
-								if (err) {
-									res.status(400).send('Query Fail');
-								} else {
-									res.send(result);
-								}			        
-							});	
-						} 
 
-						else {
-							res.status(400).send('Invalid Values');
-						}
-						
-						break;
-						
-					// Searching by phone number
-					case "phone":
-					
-						// Check if correct format
-						if(checkInput(req.body.value, req.body.type)) {
-							
-							// Search for phone number in database
-							tempCont.query("SELECT * FROM contact WHERE UserId = ? AND PhoneNumber = ?",[req.body.userid, req.body.value], function(err, result){
-								
-								// Check if query works
-								if (err) {
-									res.status(400).send('Query Fail');
-								} else {
-									res.send(result);
-								}
-							});
-					
-						} else {
-							res.status(400).send('Invalid Values');
-						}
-						
-						break;
-						
-					// Searching by email
-					default:
-					
-						// Check if correct format
-						if(checkInput(req.body.value, req.body.type)) {
-							
-							// Search for email in database
-							tempCont.query("SELECT * FROM contact WHERE UserId = ? AND Email = ?",[req.body.userid, req.body.value], function(err, result) {
-								
-								// Check if query works
-								if (err) {
-									res.status(400).send('Query Fail');
-								} else {
-									res.send(result);
-								}      
-							});
-					
-						} else {
-							res.status(400).send('Invalid Values');
-						}
-						
-						break;
-				}
-			}
+	// Create connection to database
+	db.getConnection(function(err, tempCont){
 			
-			// End connection
-			tempCont.release();
-		});
-	}
+		// Error if connection was not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else {
+				
+			switch(req.body.type) {
+					
+				// Searching by first name
+				case "firstname":
+					
+					// Check if correct format
+					if(checkInput(req.body.value, "name")) {
+							
+						// Search for first name in database
+						tempCont.query("SELECT * FROM contact WHERE UserID = ? AND FirstName LIKE '%" + req.body.value + "%'",[req.user.UserID], function(err, result) {
+								
+							// Check if query works
+							if (err) {
+								res.status(400).send('Query Fail');
+							} else {
+								res.send(result);
+							}				        
+						});				
+					
+					} else {
+						res.status(400).send('Invalid Values');
+					}	
+						
+					break;
+						
+				// Searching by last name
+				case "lastname":
+					
+					// Check if correct format
+					if(checkInput(req.body.value, "name")) {
+							
+						// Search for last name in database
+						tempCont.query("SELECT * FROM contact WHERE UserID = ? AND LastName LIKE '%" + req.body.value + "%'",[req.user.UserID], function(err, result) {
+								
+							// Check if query fail
+							if (err) {
+								res.status(400).send('Query Fail');
+							} else {
+								res.send(result);
+							}			        
+						});	
+					
+					} else {
+						res.status(400).send('Invalid Values');
+					}
+						
+					break;
+						
+				// Searching by phone number
+				case "phone":
+					
+					// Check if correct format
+					if(checkInput(req.body.value, req.body.type)) {
+							
+						// Search for phone number in database
+						tempCont.query("SELECT * FROM contact WHERE UserID = ? AND PhoneNumber LIKE '%" + req.body.value + "%'",[req.user.UserID], function(err, result){
+								
+							// Check if query works
+							if (err) {
+								res.status(400).send('Query Fail');
+							} else {
+								res.send(result);
+							}
+						});
+					
+					} else {
+						res.status(400).send('Invalid Values');
+					}
+						
+					break;
+						
+				// Searching by email
+				default:
+					
+					// Check if correct format
+					if(checkInput(req.body.value, "emailsearch")) {
+							
+						// Search for email in database
+						tempCont.query("SELECT * FROM contact WHERE UserID = ? AND Email LIKE '%" + req.body.value + "%'",[req.user.UserID], function(err, result) {
+								
+							// Check if query works
+							if (err) {
+								res.status(400).send('Query Fail');
+							} else {
+								res.send(result);
+							}      
+						});
+					
+					} else {
+						res.status(400).send('Invalid Values');
+					}
+						
+					break;
+			}
+		}
+			
+		// End connection
+		tempCont.release();
+	});
 });
 
 //Check for valid inputs
@@ -338,9 +281,18 @@ var checkInput = function(input, type, callback) {
 			var re = /^[a-z|\d]{5,20}$/i; // Format 5-20 characters and digit
 			returnVal = re.test(input);
 			break;
+
+		case "password":
+			 var re= /[a-z\d]{32}$/;
+			 returnVal= re.test(input);
 			
 		case "email":
 			var re = /^[a-z\d]{1,20}@[a-z]{1,10}(.[a-z]{3}){1,2}$/i; // Format 1-20 character @ 1-10 characters . extension
+			returnVal = re.test(input);
+			break;
+
+		case "emailsearch":
+			var re = /[[a-z\d]*@*(\.{0,1}[a-z]*)*$/i;
 			returnVal = re.test(input);
 			break;
 			
