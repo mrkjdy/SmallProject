4b1a319008cd9b9929ccab546c4af4b3ad7a9438
manu <manumanu-96@hotmail.com>
2018-09-10 17:18:12 -0400

    logout redirection

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ app.post('/login', function(req, res) {
 // logout function
 app.post('/logout', function(req, res) {
 	req.logout();
-	res.redirect('/');
+	res.redirect('/index.html');
 });
 
 // Login page
